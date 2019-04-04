768fbdf48c50405dfe6ab3d772d065a43b0ea9aa
Trang Tran Thi Ngoc <tn-trang.tran@outlook.com.vn>
2019-04-04 18:03:51 +0700

    add checkbox and color when hover on the record

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatMenuModule, MatIconModule, MatTableModule} from '@angular/material';
+import { MatButtonModule, MatMenuModule, MatIconModule, MatTableModule, MatSortModule, MatCheckboxModule} from '@angular/material';
 import { MenuComponent } from './components/menu/menu.component';
 import { DocumentsTableComponent } from './components/documents-table/documents-table.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -25,7 +25,9 @@ import { DocumentService } from './services/document.service';
     MatMenuModule, 
     MatIconModule,
     MatTableModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSortModule,
+    MatCheckboxModule
   ],
   providers: [DocumentService],
   bootstrap: [AppComponent]
--- src/app/components/documents-table/documents-table.component.ts
+++ src/app/components/documents-table/documents-table.component.ts
@@ -11,12 +11,16 @@ import { Document } from '../../models/document.model';
 })
 
 export class DocumentsTableComponent implements OnInit {
-  dataSource = new DocumentDataSource(this.documentService)
-  displayedColumns: string[] = ['imagePath', 'name', 'modified', 'modifiedBy'];
+  dataSource = new DocumentDataSource(this.documentService);
+  displayedColumns: string[] = ['checkedState', 'imagePath', 'name', 'modified', 'modifiedBy'];
 
   constructor(private documentService: DocumentService) { }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    console.log("Make sure you want to choose the record?");
   }
 }
 
@@ -31,19 +35,3 @@ export class DocumentDataSource extends DataSource<any> {
 
   disconnect() {}
 }
-
-// export interface PeriodicElement {
-//   img: string;
-//   name: string;
-//   modified: string;
-//   modifiedBy: string;
-//   addnew: string
-// }
-
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   { img: 'https://img.icons8.com/color/32/000000/ms-excel.png', name: 'CAS', modified: 'April 30', modifiedBy: 'Megan Bowen', addnew: ''},
-//   { img: 'https://img.icons8.com/color/32/000000/ms-excel.png', name: 'CoasterAndBargeLoading.xlsx', modified: 'A few seconds ago', modifiedBy: 'Adminstrator MOD', addnew: ''},
-//   { img: 'https://img.icons8.com/color/32/000000/ms-excel.png', name: 'RevenueByServices.xlsx', modified: 'A few seconds ago', modifiedBy: 'Adminstrator MOD', addnew: '' },
-//   { img: 'https://img.icons8.com/color/32/000000/ms-excel.png', name: 'RevenueByServices2016.xlsx', modified: 'A few seconds ago', modifiedBy: 'Adminstrator MOD', addnew: '' },
-//   { img: 'https://img.icons8.com/color/32/000000/ms-excel.png', name: 'RevenueByServices2017.xlsx', modified: 'A few seconds ago', modifiedBy: 'Adminstrator MOD', addnew: '' }
-// ];
